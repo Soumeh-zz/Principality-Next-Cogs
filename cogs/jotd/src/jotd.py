bbffd146a4f67b86e21818fd92df6dc9cec0f1c3
Soumeh <stefi.brezovac@gmail.com>
2022-07-27 19:39:45 +0200

    bump jotd version

--- cogs/jotd/src/jotd.py
+++ cogs/jotd/src/jotd.py
@@ -12,6 +12,5 @@ class Jester_Of_The_Day(Cog):
         await ctx.send(embed=Embed(description=f"**{ctx.guild.name}**'s Jester of the Day is {jester.mention}!"))
 
     def jester_of_the_day(self, server):
-        today = date.today()
-        seed(today)
+        seed(date.today())
         return choice(server.members)
